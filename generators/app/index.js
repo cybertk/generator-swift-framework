d5d8829efcea706dc1c50bf6a914061e460a26d0
Quanlong He <kyan.ql.he@gmail.com>
2015-08-27 17:07:39 +0800

    Create .travis.yml on the fly

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,16 +4,14 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 var path = require('path');
 var glob = require('glob');
+var yaml = require('yamljs');
 
 module.exports = yeoman.generators.Base.extend({
     prompting: function () {
         var done = this.async();
 
         // Have Yeoman greet the user.
-        this.log(yosay(
-            'Welcome to the outstanding ' + chalk.red(
-                'swift.framework') + ' generator!'
-        ));
+        this.log(yosay('Welcome to the outstanding ' + chalk.red('swift.framework') + ' generator!'));
 
         var prompts = [{
             type: 'input',
@@ -51,26 +49,25 @@ module.exports = yeoman.generators.Base.extend({
             }).forEach(function (file) {
 
                 var source = path.relative(tp, file);
+                source = source.replace(/PROJECT_NAME/g, this.props.projectName);
 
-                source = source.replace(/PROJECT_NAME/g,
-                    this.props.projectName);
-
-                this.fs.copyTpl(file, this.destinationPath(
-                    source), this.props);
+                this.fs.copyTpl(file, this.destinationPath(source), this.props);
 
             }, this);
         },
 
         license: function () {
-            this.fs.copyTpl(this.templatePath('LICENSE'), this.destinationPath(
-                'LICENSE'), this.props);
+            this.fs.copyTpl(this.templatePath('LICENSE'), this.destinationPath('LICENSE'), this.props);
         },
 
-        projectFiles: function () {
-            this.fs.copy(
-                this.templatePath('.travis.yml'),
-                this.destinationPath('.travis.yml')
-            );
+        travis: function () {
+            var script = {
+                language: 'objective-c',
+                script: [
+                    'xcodebuild test -sdk iphonesimulator -scheme ' + this.props.projectName,
+                ],
+            };
+            this.fs.write(this.destinationPath('.travis.yml'), yaml.stringify(script, 2));
         }
     },
 
