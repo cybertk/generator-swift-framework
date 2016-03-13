9d4a67e1dd4f3289ae27da6df4b073d09eb35a2b
Quanlong <kyan.ql.he@gmail.com>
2016-03-14 01:32:01 +0800

    Tests enhancement

--- test/test-app.js
+++ test/test-app.js
@@ -6,7 +6,7 @@ var path = require('path')
 var assert = require('yeoman-generator').assert
 var helpers = require('yeoman-generator').test
 
-function noFilesContent (files, pattern) {
+assert.noFilesContent = function (files, pattern) {
   files.forEach(function (file) {
     assert.noFileContent(file, pattern)
   })
@@ -16,7 +16,7 @@ describe('swift.framework:app', function () {
   // Placeholders
   var pattern = /(PROJECT-NAME|PROJECT_NAME|ORGANIZATION-ID|ORGANIZATION_NAME|<%=|%>)/
 
-  describe('with all features enabled', function () {
+  describe('with mobileprovision disable', function () {
     before(function (done) {
       helpers.run(path.join(__dirname, '../generators/app'))
         .withOptions({
@@ -40,37 +40,12 @@ describe('swift.framework:app', function () {
       var files = [
         'F.xcodeproj/project.pbxproj',
         'F.xcodeproj/project.xcworkspace/contents.xcworkspacedata',
-        // Shared scheme
-        'F.xcodeproj/xcshareddata/xcschemes/F.xcscheme'
-      ]
-      assert.file(files)
-      noFilesContent(files, pattern)
-    })
-
-    it('creates Framework target', function () {
-      // Framework target
-      var files = [
+        'F.xcodeproj/xcshareddata/xcschemes/F.xcscheme',
         'F/Info.plist',
         'F/F.h',
-        'F/F.swift'
-      ]
-      assert.file(files)
-      noFilesContent(files, pattern)
-    })
-
-    it('creates UnitTests target', function () {
-      // UnitTests target
-      var files = [
+        'F/F.swift',
         'UnitTests/Info.plist',
-        'UnitTests/UnitTests.swift'
-      ]
-      assert.file(files)
-      noFilesContent(files, pattern)
-    })
-
-    it('creates Example target', function () {
-      // UnitTests target
-      var files = [
+        'UnitTests/UnitTests.swift',
         'Example/AppDelegate.swift',
         'Example/Assets.xcassets/AppIcon.appiconset/Contents.json',
         'Example/Base.lproj/LaunchScreen.storyboard',
@@ -78,21 +53,25 @@ describe('swift.framework:app', function () {
         'Example/Info.plist',
         'Example/ViewController.swift'
       ]
+
       assert.file(files)
-      noFilesContent(files, pattern)
+      assert.noFilesContent(files, pattern)
     })
 
-    it('creates project files', function () {
-      var files = [
-        '.gitignore',
-        'script/cert',
-        'script/README.md',
-        'Makefile',
-        'Gemfile',
-        'Gemfile.lock'
-      ]
-      assert.file(files)
-      assert.noFileContent('Makefile', pattern)
+    it('creates gitignore', function () {
+      assert.file('.gitignore')
+    })
+
+    it('creates license', function () {
+      assert.file('LICENSE')
+    })
+
+    it('creates readme', function () {
+      assert.file('README.md')
+    })
+
+    it('creates contributing', function () {
+      assert.file('CONTRIBUTING.md')
     })
 
     it('creates podsepc', function () {
@@ -103,12 +82,201 @@ describe('swift.framework:app', function () {
     })
 
     it('creates Cartfile', function () {
-      assert.file('Cartfile.private')
-      assert.file('Cartfile.resolved')
+      assert.file([
+        'Cartfile.private',
+        'Cartfile.resolved'
+      ])
     })
 
     it('creates Travis scripts', function () {
       assert.file('.travis.yml')
+    })
+
+    it('creates scripts', function () {
+      var files = [
+        'script/cert',
+        'script/README.md',
+        'Makefile',
+        'Gemfile',
+        'Gemfile.lock'
+      ]
+      assert.file(files)
+      assert.noFilesContent(files, pattern)
+    })
+  })
+
+  describe('with travis disabled', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .withOptions({
+          skipInstall: true,
+          openXcode: false
+        })
+        .withPrompts({
+          projectName: 'F',
+          organizationName: 'example-org',
+          organizationId: 'org.example',
+          cocoapods: true,
+          githubUser: 'gu',
+          travis: false,
+          mobileprovision: false
+        })
+        .on('end', done)
+    })
+
+    it('creates Xcode project', function () {
+      // Xcode project
+      var files = [
+        'F.xcodeproj/project.pbxproj',
+        'F.xcodeproj/project.xcworkspace/contents.xcworkspacedata',
+        'F.xcodeproj/xcshareddata/xcschemes/F.xcscheme',
+        'F/Info.plist',
+        'F/F.h',
+        'F/F.swift',
+        'UnitTests/Info.plist',
+        'UnitTests/UnitTests.swift',
+        'Example/AppDelegate.swift',
+        'Example/Assets.xcassets/AppIcon.appiconset/Contents.json',
+        'Example/Base.lproj/LaunchScreen.storyboard',
+        'Example/Base.lproj/Main.storyboard',
+        'Example/Info.plist',
+        'Example/ViewController.swift'
+      ]
+
+      assert.file(files)
+      assert.noFilesContent(files, pattern)
+    })
+
+    it('creates gitignore', function () {
+      assert.file('.gitignore')
+    })
+
+    it('creates license', function () {
+      assert.file('LICENSE')
+    })
+
+    it('creates readme', function () {
+      assert.file('README.md')
+    })
+
+    it('creates contributing', function () {
+      assert.file('CONTRIBUTING.md')
+    })
+
+    it('creates podsepc', function () {
+      assert.file('F.podspec')
+      // repo url
+      assert.fileContent('F.podspec', 'https://github.com/gu/F.git')
+      assert.noFileContent('F.podspec', pattern)
+    })
+
+    it('creates Cartfile', function () {
+      assert.file([
+        'Cartfile.private',
+        'Cartfile.resolved'
+      ])
+    })
+
+    it('does not create Travis scripts', function () {
+      assert.noFile('.travis.yml')
+    })
+
+    it('creates scripts', function () {
+      var files = [
+        'script/cert',
+        'script/README.md',
+        'Makefile',
+        'Gemfile',
+        'Gemfile.lock'
+      ]
+      assert.file(files)
+      assert.noFilesContent(files, pattern)
+    })
+  })
+
+  describe('with cocoapods disabled', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .withOptions({
+          skipInstall: true,
+          openXcode: false
+        })
+        .withPrompts({
+          projectName: 'F',
+          organizationName: 'example-org',
+          organizationId: 'org.example',
+          cocoapods: false,
+          githubUser: 'gu',
+          travis: true,
+          mobileprovision: false
+        })
+        .on('end', done)
+    })
+
+    it('creates Xcode project', function () {
+      // Xcode project
+      var files = [
+        'F.xcodeproj/project.pbxproj',
+        'F.xcodeproj/project.xcworkspace/contents.xcworkspacedata',
+        'F.xcodeproj/xcshareddata/xcschemes/F.xcscheme',
+        'F/Info.plist',
+        'F/F.h',
+        'F/F.swift',
+        'UnitTests/Info.plist',
+        'UnitTests/UnitTests.swift',
+        'Example/AppDelegate.swift',
+        'Example/Assets.xcassets/AppIcon.appiconset/Contents.json',
+        'Example/Base.lproj/LaunchScreen.storyboard',
+        'Example/Base.lproj/Main.storyboard',
+        'Example/Info.plist',
+        'Example/ViewController.swift'
+      ]
+
+      assert.file(files)
+      assert.noFilesContent(files, pattern)
+    })
+
+    it('creates gitignore', function () {
+      assert.file('.gitignore')
+    })
+
+    it('creates license', function () {
+      assert.file('LICENSE')
+    })
+
+    it('creates readme', function () {
+      assert.file('README.md')
+    })
+
+    it('creates contributing', function () {
+      assert.file('CONTRIBUTING.md')
+    })
+
+    it('does not create podsepc', function () {
+      assert.noFile('F.podspec')
+    })
+
+    it('creates Cartfile', function () {
+      assert.file([
+        'Cartfile.private',
+        'Cartfile.resolved'
+      ])
+    })
+
+    it('creates Travis scripts', function () {
+      assert.file('.travis.yml')
+    })
+
+    it('creates scripts', function () {
+      var files = [
+        'script/cert',
+        'script/README.md',
+        'Makefile',
+        'Gemfile',
+        'Gemfile.lock'
+      ]
+      assert.file(files)
+      assert.noFilesContent(files, pattern)
     })
   })
 })
